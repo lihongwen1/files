93e924fb51421c1dcba0bdccb92f9722ee47464a
lihongwen1 <35321394+lihongwen1@users.noreply.github.com>
2023-04-27 13:55:05 +0800

    Create ClearEditText.java

---
+++ ClearEditText.java
@@ -0,0 +1,112 @@
+package com.sz.verefa.sweeprobot.view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.sz.verefa.sweeprobot.R;
+
+
+public class ClearEditText extends AppCompatEditText {
+    /**
+     * 删除按钮的引用
+     */
+    private Drawable mClearDrawable;
+
+    /**
+     * 控件是否有焦点
+     */
+    private boolean hasFocus;
+
+    public ClearEditText(Context context) {
+        this(context, null);
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.editTextStyle);
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        //获取EditText的DrawableRight,假如没有设置我们就使用默认的图片
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            Resources res = getResources();
+            mClearDrawable = new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.mipmap.replace));
+        }
+        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+        //默认设置隐藏图标
+        setClearIconVisible(false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mClearDrawable != null && event.getAction() == MotionEvent.ACTION_UP) {
+            int x = (int) event.getX();
+            //判断触摸点是否在水平范围内
+            boolean isInnerWidth = (x > (getWidth() - getTotalPaddingRight())) && (x < (getWidth() - getPaddingRight()));
+            //获取删除图标的边界，返回一个Rect对象
+            Rect rect = mClearDrawable.getBounds();
+            //获取删除图标的高度
+            int height = rect.height();
+            int y = (int) event.getY();
+            //计算图标底部到控件底部的距离
+            int distance = (getHeight() - height) / 2;
+            //判断触摸点是否在竖直范围内(可能会有点误差)
+            //触摸点的纵坐标在distance到（distance+图标自身的高度）之内，则视为点中删除图标
+            boolean isInnerHeight = (y > distance) && (y < (distance + height));
+            if (isInnerHeight && isInnerWidth) {
+                this.setText("");
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 设置清除图标的显示与隐藏，调用setCompoundDrawables为EditText绘制上去
+     *
+     * @param visible
+     */
+    private void setClearIconVisible(boolean visible) {
+        Drawable right = visible ? mClearDrawable : null;
+        setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
+    }
+
+    /**
+     * 当ClearEditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
+     */
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        this.hasFocus = focused;
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+    }
+
+    /**
+     * 当输入框里面内容发生变化的时候回调的方法
+     */
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        if (hasFocus) {
+            setClearIconVisible(text.length() > 0);
+        }
+    }
+}
